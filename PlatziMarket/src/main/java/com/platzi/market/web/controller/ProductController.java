1b5f74d2dc723001c7c94004fde8628d102edf8e
cesar contreras <contreras.cesar53@gmail.com>
2022-09-23 15:42:52 -0500

    Se añade documentacion con swagger2

--- PlatziMarket/src/main/java/com/platzi/market/web/controller/ProductController.java
+++ PlatziMarket/src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -3,6 +3,10 @@ package com.platzi.market.web.controller;
 import java.util.List;
 
 import com.platzi.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +22,19 @@ public class ProductController {
 	private ProductService productService;
 
 	@GetMapping("/all")
+	@ApiOperation("Get all supermarket products") /**Anotacion para documentar swagger */
+	@ApiResponse(code = 200, message = "Ok") /**Anotacion para documentar swagger */
 	public ResponseEntity<List<Product>> getAll(){
 		return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Product> getProduct(@PathVariable("id") int productId){
+	@ApiOperation("Search a product by ID") /**Anotacion para documentar swagger */
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "Ok"), /**Anotacion para documentar swagger */
+			@ApiResponse(code = 404, message = "Product not fount") /**Anotacion para documentar swagger */
+	})
+	public ResponseEntity<Product> getProduct(@ApiParam(value = "The Id of the product", required = true, example = "7") @PathVariable("id") int productId){
 		return productService.getProduct(productId).map(product -> new ResponseEntity<>(product, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 
